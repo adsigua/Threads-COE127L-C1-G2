0e9624a423b2cb886bc9b8a92ab24ec874a51051
iangabrielle <COE127L-C1-05>
2018-10-06 18:31:23 +0800

    Added wakeTicks

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -15,6 +15,11 @@
 #include "userprog/process.h"
 #endif
 
+/*
+THIS IS A TEST
+*/
+
+
 /* Random value for struct thread's `magic' member.
    Used to detect stack overflow.  See the big comment at the top
    of thread.h for details. */
